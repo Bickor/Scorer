93ab323eb5410077768419f6a4a100d16e583b50
Martin Heberling <martin.heberling.dalies@gmail.com>
2021-08-12 11:42:23 -0500

    Can now grab input from multiple fields and all fields must be full when submit.

--- src/Score.js
+++ src/Score.js
@@ -4,65 +4,45 @@ class Score extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {scorer: "",
-                      scorers: []};
+        this.state = {
+                    player1: "",
+                    score1: "",
+                    player2: "",
+                    score2: "",
+                    scorers: []};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handlePlayer1 = this.handlePlayer1.bind(this);
+        this.handlePlayer2 = this.handlePlayer2.bind(this);
+        this.handleScore1 = this.handleScore1.bind(this);
+        this.handleScore2 = this.handleScore2.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        //this.appendData = this.appendData.bind(this);
-        //this.appendWrittenData = this.appendWrittenData.bind(this);
-    }
-    /**
-    state = {
-        scores: [{
-            name1: "German",
-            score1: "50",
-            name2: "Martin",
-            score2: "37"
-        }]
-    }
-    
-
-    appendData = () => {
-        const newData = {
-            name1: "German",
-            score1: "50",
-            name2: "Martin",
-            score2: "70"
-        }
-        this.setState(prevState => ({scores: [...prevState.scores, newData]}))
     }
 
-    appendWrittenData = (event) => {
-        const newData = {
-            name1: "Silvana",
-            score1: "30",
-            name2: "Martin",
-            score2: "20"
-        }
-        this.setState(prevState => ({scores: [...prevState.scores, newData]}))
-    }
-    */
-
-    handleChange(event) {
-        this.setState({scorer: event.target.value});
-        // console.log(event.target.value);
-        //event.preventDefault();
+    handlePlayer1(event) {
+        this.setState({player1: event.target.value});
     }
 
+    handleScore1(event) {
+        this.setState({score1: event.target.value});
+    }
+
+    handlePlayer2(event) {
+        this.setState({player2: event.target.value});
+    }
+
+    handleScore2(event) {
+        this.setState({score2: event.target.value});
+    }
     
     handleSubmit(event) {
-        this.setState(prevState => ({scorers: [...prevState.scorers, this.state.scorer]}))
-        console.log(this.state.scorer);
+        if (this.state.player1 !== ""
+            && this.state.score1 !== ""
+            && this.state.player2 !== ""
+            && this.state.score2 !== "") {
+            this.setState(prevState => ({scorers: [...prevState.scorers, [this.state.player1, this.state.score1, this.state.player2, this.state.score2]]}))
+        }
         event.preventDefault();
     }
-
-    /**
-     {this.state.scorer.map((score, index) => {
-                    return (<p key={index}>{score}</p>);
-                    //return (<p key={index}>{score.name1} {score.score1} - {score.name2} {score.score2}</p>);
-                    })}
-     */
     
 
     render() {
@@ -70,16 +50,22 @@ class Score extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        Name: <input type="text" value={this.state.scorer} onChange={this.handleChange}/>
+                        Player 1: <input type="text" value={this.state.player1} onChange={this.handlePlayer1}/>
+                    </label>
+                    <label>
+                        Score 1: <input type="text" value={this.state.score1} onChange={this.handleScore1}/>
+                    </label>
+                    <label>
+                        Player 2: <input type="text" value={this.state.player2} onChange={this.handlePlayer2}/>
+                    </label>
+                    <label>
+                        Score 2: <input type="text" value={this.state.score2} onChange={this.handleScore2}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
-                <button onClick={this.appendData}>Add Score</button>
                 {this.state.scorers.map((score, index) => {
-                    return (<p key={index}>{score}</p>)
+                    return (<p key={index}>{score[0]} {score[1]} - {score[3]} {score[2]}</p>)
                 })}
-                
-                
             </div>
         );
     }
